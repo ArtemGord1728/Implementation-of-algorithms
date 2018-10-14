e6447ac65bd31e4d2383927e9f29396030839af3
ArtemGord1728 <artemseraf01@mail.ru>
2018-10-14 13:21:26 +0300

    Fixed bugs

--- BinarySearch/Binary_Search_Main.py
+++ BinarySearch/Binary_Search_Main.py
@@ -1,18 +1,18 @@
 def binary_search(list, item):
     low = 0
     high = len(list) - 1
-    mid = (low + high)
-    guess_item = list[mid]
 
-    if guess_item == item:
-        return item
-    if guess_item > item:
-        high = mid - 1
-    else:
-        low = mid + 1
-
+    while low <= high:
+        mid = (low + high)
+        guess_item = list[mid]
+        if guess_item == item:
+            return item
+        if guess_item > item:
+            high = mid - 1
+        else:
+            low = mid + 1
     return None
 
 items = [1, 2, 3, 4]
 
-print(binary_search(items, 2))
+print(binary_search(items, 4))
