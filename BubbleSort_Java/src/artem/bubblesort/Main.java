56f551d82bf187460c7d1c2d558a55d62cd1d62d
ArtemGord1728 <artemseraf01@mail.ru>
2018-11-09 21:54:23 +0300

    Refactor #

--- BubbleSort_Java/src/artem/bubblesort/Main.java
+++ BubbleSort_Java/src/artem/bubblesort/Main.java
@@ -6,6 +6,7 @@ public class Main {
     {
         int mas[] = {1, 2, 4, 3};
         BubbleSort_Class b_sort = new BubbleSort_Class();
+        b_sort.PrintArray(mas);
         b_sort.Sort(mas);
     }
 }
