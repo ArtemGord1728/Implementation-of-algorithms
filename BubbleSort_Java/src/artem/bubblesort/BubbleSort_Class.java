2aad62a4f996a711ea2c9f609a4e4c74173b7ffd
ArtemGord1728 <artemseraf01@mail.ru>
2018-11-09 21:50:37 +0300

    Added new functionallity

--- BubbleSort_Java/src/artem/bubblesort/BubbleSort_Class.java
+++ BubbleSort_Java/src/artem/bubblesort/BubbleSort_Class.java
@@ -2,21 +2,44 @@ package artem.bubblesort;
 
 public class BubbleSort_Class
 {
-    public void swap(int array[], int right, int left)
-    {
-        if(right < left)
-        {
+    private void swap(int[] array, int left, int right) {
+        if (left > right) {
             int item = array[left];
             array[left] = array[right];
             array[right] = item;
         }
     }
 
-    public void Sort(int main_array[])
+    public void PrintArray(int[] arr)
     {
-        for(int x = 0; x < main_array.length - 1; x++)
+        for (int i = 0; i < arr.length; i++)
         {
-            swap(main_array, x - 1, x + 1);
+            System.out.println("Первоначальный массив - " + arr[i]);
+        }
+        System.out.println();
+    }
+
+
+    public void Sort(int[] main_array)
+    {
+        boolean isSwapped = false;
+        do {
+            for (int x = 0; x < main_array.length - 1; x++) {
+
+                if(main_array[x] > main_array[x + 1]) {
+                    isSwapped = true;
+                    swap(main_array, x + 1, x);
+                }
+                System.out.println(main_array[x]);
+            }
+        }while (!isSwapped);
+    }
+
+    public void ReadyArray(int[] arr)
+    {
+        for (int i = 0; i < arr.length; i++)
+        {
+            System.out.println("Отсортированный массив - " + arr[i]);
         }
     }
 }
