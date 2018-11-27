734a25b1db5a3a462ad65ff3d2a2bc0de8f2b1e9
ArtemGord1728 <artemseraf01@mail.ru>
2018-11-27 23:02:06 +0300

    Start implement binary search

---
+++ BinarySearch_C++/BinarySearch_C++/BinarySearch_C++.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class BinarySearch
+{
+public:
+	void sort_array();
+	void binary_search();
+};
---
+++ BinarySearch_C++/BinarySearch_C++/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "ru");
+
+
+
+	system("pause");
+	return 0;
+}
