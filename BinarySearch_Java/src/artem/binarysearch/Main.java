e9d3f769118b30493d6da4dfefab05f2487a3beb
ArtemGord1728 <artemseraf01@mail.ru>
2018-11-27 23:38:51 +0300

    Changes

--- BinarySearch_Java/src/artem/binarysearch/Main.java
+++ BinarySearch_Java/src/artem/binarysearch/Main.java
@@ -1,15 +1,12 @@
 package artem.binarysearch;
 
-
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-	    int[] arr = {1, 2, 3, 5};
-        Scanner scanner = new Scanner(System.in);
-        int elem = scanner.nextInt(arr.length);
+	    int[] arr = {1, 2, 3, 4};
+	    int elem = 3;
         BinarySearch binarySearch = new BinarySearch();
-        binarySearch.searchItem(arr, elem);
+        int res = binarySearch.searchItem(arr, elem);
+        System.out.println(res);
     }
 }
