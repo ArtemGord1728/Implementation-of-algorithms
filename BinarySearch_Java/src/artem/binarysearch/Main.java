7d6e221ae40fa595b5561a61ec49ae3c9b73896f
ArtemGord1728 <artemseraf01@mail.ru>
2018-11-28 19:50:30 +0300

    Rewrited #2

--- BinarySearch_Java/src/artem/binarysearch/Main.java
+++ BinarySearch_Java/src/artem/binarysearch/Main.java
@@ -4,9 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	    int[] arr = {1, 2, 3, 4};
-	    int elem = 3;
         BinarySearch binarySearch = new BinarySearch();
-        int res = binarySearch.searchItem(arr, elem);
-        System.out.println(res);
+        binarySearch.searchItem(arr, 2);
     }
 }
