417901776e70f4a4879fdd004fe5f6b42582ad6f
ArtemGord1728 <artemseraf01@mail.ru>
2018-11-28 19:54:25 +0300

    Fixed bugs

--- BinarySearch_Java/src/artem/binarysearch/BinarySearch.java
+++ BinarySearch_Java/src/artem/binarysearch/BinarySearch.java
@@ -1,26 +1,24 @@
 package artem.binarysearch;
 
-public class BinarySearch
-{
-    public int searchItem(int[] listItems, int item)
-    {
+public class BinarySearch {
+    public void searchItem(int[] listItems, int item) {
         int lowElem = 0;
         int highElem = (listItems.length) - 1;
 
-        while (lowElem <= highElem)
-        {
+        while (lowElem <= highElem) {
+
             int middle = (lowElem + highElem) / 2;
             int guessItem = listItems[middle];
 
-            if(guessItem == item)
-                return item;
+            if (guessItem == item) {
+                System.out.println("Index of value " + item + " equals " + middle);
+                return;
+            }
 
-            if(guessItem < item)
+            if (guessItem < item)
                 lowElem = middle - 1;
             else
                 highElem = middle + 1;
         }
-
-        return 0;
     }
 }
