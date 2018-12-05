1a4d6449d804c457efd8318213131b3bd9cef947
ArtemGord1728 <artemseraf01@mail.ru>
2018-12-05 21:08:13 +0300

    Important changes

--- Stack_Java/src/enclave/gord/stackjava/Stack.java
+++ Stack_Java/src/enclave/gord/stackjava/Stack.java
@@ -1,4 +1,28 @@
 package enclave.gord.stackjava;
 
-public class Stack {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stack<T>
+{
+    private List<Object> elementsList = new ArrayList<>();
+    private int topOfStack;
+
+    public Stack(List<Object> elementsList)
+    {
+        this.elementsList = elementsList;
+    }
+
+    public Stack() {
+    }
+
+    public void add(Object element){
+        topOfStack++;
+        elementsList.add(element);
+    }
+
+    public boolean empty()
+    {
+        return elementsList.isEmpty();
+    }
 }
