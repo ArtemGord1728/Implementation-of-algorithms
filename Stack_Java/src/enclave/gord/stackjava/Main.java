673ec8c30f7e4c450e0c0dd3feff453c53cabb41
ArtemGord1728 <artemseraf01@mail.ru>
2018-12-06 18:19:24 +0300

    Test

--- Stack_Java/src/enclave/gord/stackjava/Main.java
+++ Stack_Java/src/enclave/gord/stackjava/Main.java
@@ -1,8 +1,16 @@
 package enclave.gord.stackjava;
 
-public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main
+{
+    // Test
+    public static void main(String[] args)
+    {
+        Stack<Integer> s = new Stack<>();
+        s.add(4);
+        s.add(2);
+        s.add(3);
+        s.add(3);
+
     }
 }
