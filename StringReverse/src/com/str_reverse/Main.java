734b323b6701426e0ad639f1348a4f4df679db74
ArtemGord1728 <artemgord00@gmail.com>
2019-08-09 19:35:30 +0300

    modified code

--- StringReverse/src/com/str_reverse/Main.java
+++ StringReverse/src/com/str_reverse/Main.java
@@ -4,10 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         String c = "hello";
-        int i;
         String temp = "";
 
-        for (i = c.length() - 1; i >= 0; i--){
+        for (int i = c.length() - 1; i >= 0; i--){
             temp += c.substring(i, i + 1);
         }
         System.out.println(temp);
